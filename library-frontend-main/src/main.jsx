82a953cc4d7747d99e9c16b69a52add65bb5611e
ville24 <73233385+ville24@users.noreply.github.com>
2024-06-14 10:32:16 +0300

    Create main.jsx

---
+++ library-frontend-main/src/main.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+} from '@apollo/client'
+
+const client = new ApolloClient({
+  /*uri: 'http://localhost:4000',*/
+  uri: 'https://didactic-train-wx6prvp5vp7c5569-4000.app.github.dev/',
+  /*credentials: 'same-origin',*/
+  cache: new InMemoryCache(),
+})
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+
+  <ApolloProvider client={client}>
+    <App />
+
+  </ApolloProvider>
+)
